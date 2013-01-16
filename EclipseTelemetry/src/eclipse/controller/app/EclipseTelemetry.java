301b6944cdde34e2c0ccda507c5d3f5ef03ab11d
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2013-01-16 17:29:21 +0000

    now use thredGen

--- EclipseTelemetry/src/eclipse/controller/app/EclipseTelemetry.java
+++ EclipseTelemetry/src/eclipse/controller/app/EclipseTelemetry.java
@@ -3,7 +3,7 @@ package eclipse.controller.app;
 /**
  * Eclipse Telemetry, Start Point, Where you can find the main of this application. This application, 
  * receive data from solar car and show them in different windows.
- * s
+ * 
  * This telemetry is used for Eclipse solar car 8.
  * 
  * ETS, École de Technologie suppérieure.
@@ -12,12 +12,26 @@ package eclipse.controller.app;
  *
  */
 public class EclipseTelemetry {
+	
+	private static ThreadGen threadGenInstance;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		
+		//init threadGen
+		threadGenInstance = ThreadGen.getInstance();
+		
+		
+		initApp();
+	}
+	
+	private static void initApp(){
+		
+		
+		//Start the ThreadGenerator
+		threadGenInstance.startThread();
 	}
 
 }
