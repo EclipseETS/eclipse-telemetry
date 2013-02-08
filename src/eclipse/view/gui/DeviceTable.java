6afc556a79c717bee6770e69308c17c0b33b0263
Fabrice Houle <fabrice.houle@gmail.com>
2013-02-08 04:12:56 +0000

    COmmencer a coder DeviceTable

--- src/eclipse/view/gui/DeviceTable.java
+++ src/eclipse/view/gui/DeviceTable.java
@@ -1,27 +1,84 @@
 package eclipse.view.gui;
 
+import java.util.Observable;
+import java.util.Observer;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
+import javax.swing.table.AbstractTableModel;
 
-public class DeviceTable extends JPanel {
+import eclipse.model.data.DataManager;
+import eclipse.model.data.Device;
+
+/**
+ * This class is used to create the table that displays the car data
+ * @author fabricehoule
+ *
+ */
+
+public class DeviceTable extends JPanel implements Observer {
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -2652127495341433024L;
-	private JTable table;
+	private JTable dataTable;
+	private DataManager dataManager = DataManager.getInstance();
 
 	/**
-	 * Create the panel.
+	 * Creates the panel
 	 */
 	public DeviceTable() {
 		
-		table = new JTable();
+		dataTable = new JTable(new DeviceTableModel());
 		
-		
-		JScrollPane scrollPane = new JScrollPane(table);
-		this.add(scrollPane);
+		// Creates a scroll pane as a container for the table
+		JScrollPane scrollPane = new JScrollPane(dataTable);
+		dataTable.setFillsViewportHeight(true);
 
 	}
 
+	/**
+	 * This method is called whenever an object is changed
+	 */
+	public void update(Observable o, Object arg) {
+		
+		
+	}
+
+	/**
+	 * Class for creating a new table model
+	 * @author fabricehoule
+	 *
+	 */
+	class DeviceTableModel extends AbstractTableModel {
+	
+		private String[] columnNames = {"Device", "ID", "Item", "Value", "Last seen"};
+
+		public int getRowCount() {
+			//TODO Methods for the table model
+			
+			int numberOfItems = 0;
+			//for (dataManager.getDevices(){
+			//	numberOfItems += dataManager.getDevices().size();
+			//}
+				
+			return 0;
+		}
+
+		@Override
+		public int getColumnCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	
 }
