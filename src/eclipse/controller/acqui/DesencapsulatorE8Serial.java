e6361dd8a9a357654da95942a9c0ab65ad85bc70
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2013-02-25 22:38:23 +0000

    fin de journée men va au gym, changer des truc dans le fichier properties pi toute avance

--- src/eclipse/controller/acqui/DesencapsulatorE8Serial.java
+++ src/eclipse/controller/acqui/DesencapsulatorE8Serial.java
@@ -15,14 +15,11 @@ public class DesencapsulatorE8Serial implements Desencapsulator {
 	static Logger logger = Logger.getLogger("telemetry");
 	private int value = 0x30;
 	
+	
 	public void receiveChar(Byte bt) {
 		if(bt==value){
 			if(byteArray[0]==value)
-				
-				//manipuler les données (Frame devrais etre entier)
-				for(int i=0;i<cpt;i++)				
-					System.out.println(byteArray[i]);
-			
+				parseData();		
 			cpt=0;
 		}
 		byteArray[cpt]=bt;
@@ -32,6 +29,15 @@ public class DesencapsulatorE8Serial implements Desencapsulator {
 	
 	public void clearData() {
 		byteArray = new Byte[15];
+	}
+	
+	private void parseData(){
+//		//manipuler les données (Frame devrais etre entier)
+//		for(int i=0;i<cpt;i++)				
+//			System.out.println(byteArray[i]);
+		
+		
+		
 	}
 
 }
