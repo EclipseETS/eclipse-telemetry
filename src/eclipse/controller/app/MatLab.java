c5603416d4c9796edb73db18994ed54f7b644532
Jean-Francois Chenier <j.f_chenier@hotmail.com>
2016-06-17 21:16:39 -0400

    fix matlab output

--- src/eclipse/controller/app/MatLab.java
+++ src/eclipse/controller/app/MatLab.java
@@ -22,29 +22,21 @@ import eclipse.model.data.DataManager;
  */
 public class MatLab implements Runnable{
 	
-	private static final int DRIVE_ID = 2;
-	private static final int BMS_ID = 3;
-	private static final int INSTRU_ID = 6;
-	private static final int PSU_ID = 7;
 	
-	private static final int DRIVE_ABUS_ID = 7;
-	private static final int DRIVE_VBUS_ID = 8;
-	private static final int DRIVE_RPM_ID = 10;
+	private static final int VOLANT_ID = 7;
 	
-	private static final int BMS_AH_ID = 47;
-	private static final int BMS_PACKA_ID = 72;
-	private static final int BMS_PACKV_ID = 73;
-	
-	private static final int INSTRU_LAT_ID = 2;
-	private static final int INSTRU_LON_ID = 3;
-	private static final int INSTRU_TIME_ID = 4;
-	
-	private static final int PSU_AHP_ID = 4;
-	private static final int PSU_VHP_ID = 5;
+	private static final int VOLANT_VVELOCITY_ID = 4;
+	private static final int VOLANT_MVELOCITY_ID = 5;
+	private static final int VOLANT_BUSVOLTAGE_ID = 6;
+	private static final int VOLANT_BUSCURRENT_ID = 7;
+	private static final int VOLANT_ODOMETER_ID = 8;
+	private static final int VOLANT_AMPHOUR_ID = 9;
 
 	private String sep = ",";
 	
 	public void run() {
+		
+
 		DataManager dd = DataManager.getInstance();
 		FileWriter fstream;
 		BufferedWriter out;
@@ -60,20 +52,19 @@ public class MatLab implements Runnable{
 		    while(true){
 		    	if(DataAcquisition.getInstance().getAcquiStatus()){
 			    	fstream = new FileWriter(dir.getPath() + "/" + TelemetrySettings.getInstance().getSetting("MATLABFILE")+".dat",true);
+			        Date date = new Date();
+
 			    	out = new BufferedWriter(fstream);
 						 out.write(
-			    			dd.getRawValue(INSTRU_ID, INSTRU_TIME_ID)+sep+//HEURE
-			    			dd.getRawValue(INSTRU_ID, INSTRU_LAT_ID)+sep+//LAT
-			 				dd.getRawValue(INSTRU_ID, INSTRU_LON_ID)+sep+//LON
-			 				((dd.getRawValue(BMS_ID, BMS_PACKA_ID) * dd.getRawValue(BMS_ID, BMS_PACKV_ID)) - 
-							(dd.getRawValue(DRIVE_ID, DRIVE_ABUS_ID) * dd.getRawValue(DRIVE_ID, DRIVE_VBUS_ID)) - 
-							(dd.getRawValue(PSU_ID, PSU_AHP_ID) * dd.getRawValue(PSU_ID, PSU_VHP_ID)))+sep+//PANEL_POWER
-			 				dd.getRawValue(DRIVE_ID, DRIVE_RPM_ID)+sep+//DRIVE_RPM
-			 				dd.getRawValue(DRIVE_ID, DRIVE_ABUS_ID)+sep+//DRIVE_C
-			 				dd.getRawValue(DRIVE_ID, DRIVE_VBUS_ID)+sep+//DRIVE_T
-			 				dd.getRawValue(BMS_ID, BMS_AH_ID)+sep+//BMS_AH
-			    			dd.getRawValue(BMS_ID, BMS_PACKA_ID)+sep+//BMS_COURANT
-			    			dd.getRawValue(BMS_ID, BMS_PACKV_ID)+//BMS_TENSION
+							date.toString()+sep+//HEURE
+			    			dd.getRawValue(VOLANT_ID, VOLANT_VVELOCITY_ID)+sep+//LAT
+			 				dd.getRawValue(VOLANT_ID, VOLANT_MVELOCITY_ID)+sep+//LON
+			 				dd.getRawValue(VOLANT_ID, VOLANT_BUSVOLTAGE_ID)+sep+//DRIVE_RPM
+			 				dd.getRawValue(VOLANT_ID, VOLANT_BUSCURRENT_ID)+sep+//DRIVE_C
+			 				dd.getRawValue(VOLANT_ID, VOLANT_ODOMETER_ID)+sep+//DRIVE_T
+			 				dd.getRawValue(VOLANT_ID, VOLANT_AMPHOUR_ID)+//DRIVE_T
+
+			    			
 			    			"\r\n"
 			 				);
 						 out.close();
